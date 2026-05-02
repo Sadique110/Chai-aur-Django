e163d6512e5149782f22b63e0daebed175f704ba
mohammad sadique <mohammadsadique110786@gmail.com>
2026-05-02 17:43:02 +0530

    Add chai model and template fixes

--- ChaiORDjango/chai/admin.py
+++ ChaiORDjango/chai/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ChaiVerities
 
 # Register your models here.
+admin.site.register(ChaiVerities)
+
+def __str__(self):
+    return self.name
--- ChaiORDjango/chai/models.py
+++ ChaiORDjango/chai/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class ChaiVerities(models.Model):
+    CHAI_TYPES = [
+        ("Masala Chai", "Masala Chai"),
+        ("Ginger Chai", "Ginger Chai"),
+        ("Cardamom Chai", "Cardamom Chai"),
+        ("Tulsi Chai", "Tulsi Chai"),
+        ("Lemon Chai", "Lemon Chai"),
+    ]
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.ImageField(upload_to="chais/")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    type = models.CharField(max_length=50, choices=CHAI_TYPES)
+
