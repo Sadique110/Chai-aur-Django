8bfb5932909ad9c26efa2857c48794790c1b058f
unknown <mohammadsadique110786@gmail.com>
2026-05-08 23:27:32 +0530

    your message

---
+++ chai/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def all_chai(request):
+    return render(request, 'chai/all_chai.html')
+def green_tea(request):
+    return render(request, 'green_tea.html' )
